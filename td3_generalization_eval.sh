8f6f6cea7945d5b6e8fcd0f6d6d1eb73dd4c23fb
ChrisA <acthegreat133@gmail.com>
2023-12-16 12:16:59 -0800

    Cleanup part 1

--- td3_generalization_eval.sh
+++
@@ -1,7 +0,0 @@
-# Example script to run a batch of TD3 generalization experiments
-for num in 1 5 10 20 50 100; do
-  python eval_td3.py \
-  --log-dir MetaDrive-Tut-${num}Env-v0/td3 \
-  --num-envs 10 \
-  --num-episodes 100
-done
